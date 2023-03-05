a5f696725df0ff862f6a9a01e3c87df39c737fd5
Daniel Suhett <danysuhett@hotmail.com>
2023-03-05 14:10:52 -0300

    sync main function

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod guess;
 mod square;
 mod explore_enum;
 mod two_sum;
-
-
+mod vec;
+mod hash_map;
 
 fn main() {
     // guess::new_game();
     // square::draw(32, String::from("#"));
     // area::calculate(10, 2);
     two_sum::two_sum([2,7,11,15].to_vec(), 9);
+    // vec::vec();
+    // hash_map::hash_map();
 }
